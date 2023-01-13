f532e7c2541c0369c4f7a0a561133162910642a3
Hamidou TESSILIMI <hamidou.tessilimi@epitech.eu>
2023-01-13 19:30:47 +0100

    update number_template

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -39,8 +39,7 @@ def display_number(n):
 
 @app.route("/number_template/<int:n>", strict_slashes=False)
 def int_template(n):
-    """only display when n is integer
-    """
+    """Display a HTML page only if n is an integer"""
     return render_template('5-number.html', number=n)
 
 
